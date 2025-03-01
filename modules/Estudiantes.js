da47f366589810b69bf01ff7b6ede4c06c997814
Ramirez376 <160615555+Ramirez376@users.noreply.github.com>
2025-03-01 09:23:25 -0400

    Add files via upload

---
+++ modules/Estudiantes.js
@@ -0,0 +1,10 @@
+let contadorId = 1;
+class Estudiante {
+  constructor(nombre, edad, nivel) {
+    this.id = contadorId++;
+    this.nombre = nombre;
+    this.edad = edad;
+    this.nivel = nivel;
+  }
+}
+export default Estudiante;
